4e17e5a6db9fc6dd626e5b00ef5159e53f54be63
Dominic Watson <dominic.watson@xcaliber.com>
2018-01-29 14:18:31 +0100

    Add debug info to demo app

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MenuComponent } from 'ionic-split-pane';
+import { merge } from 'rxjs/Observable/merge';
 
 @Component({
   selector: 'demo-app',
   template: `
     <ion-split-pane when="lg">
-      <ion-menu [content]="content">
-        Menu
+      <ion-menu #menu [content]="content">
+        <div>Menu</div>
+        <div>
+          <b>Width:</b> {{ menu.width() }}
+        </div>
+        <div>
+          <b>Content translateX:</b> {{ transform }}
+        </div>
       </ion-menu>
       <section #content>
         <header>
@@ -16,4 +24,23 @@ import { Component } from '@angular/core';
     </ion-split-pane>
   `,
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  @ViewChild('menu') menu: MenuComponent;
+  @ViewChild('content') content: ElementRef;
+
+  transform: string;
+
+  constructor(private changeRef: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    merge(this.menu.ionOpen, this.menu.ionClose)
+      .subscribe(() => this.setTransform());
+  }
+
+  @HostListener('document:orientationchange')
+  setTransform() {
+    this.transform = this.content.nativeElement.style.transform;
+    this.changeRef.detectChanges();
+  }
+}
